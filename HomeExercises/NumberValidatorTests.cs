2d2d7c14b587ae1008628055f81aa332e6b7d694
Anton Zotov <azpk360@yandex.ru>
2017-10-25 14:53:57 +0500

    Переделано на TestCase

--- HomeExercises/NumberValidatorTests.cs
+++ HomeExercises/NumberValidatorTests.cs
@@ -5,165 +5,103 @@ using NUnit.Framework;
 
 namespace HomeExercises
 {
-	public class NumberValidatorTests
-	{
-        private NumberValidator testValidator_3_2 { get; set; }
-        private NumberValidator testValidator_17_2{ get; set; }
-        private NumberValidator testValidator_17_2_true{ get; set; }
+    [TestFixture]
+    public class NumberValidatorTests
+    {
+        [TestCase(0, 0, false, "precision must be a positive number", 
+            TestName = "When precision is zero or less")]
+        [TestCase(1, -1, false, "precision must be a non-negative number less or equal than precision", 
+            TestName = "When scale is less then zero")]
+        [TestCase(1, 1, false, "precision must be a non-negative number less or equal than precision", 
+            TestName = "When scale is more then precision")]
 
-        [SetUp]
-        public void SetUp()
+        public void Constructor_ThrowException(int precision, int scale, bool onlyPositive, string exMessage)
         {
-            testValidator_3_2 = new NumberValidator(3, 2);
-            testValidator_17_2 = new NumberValidator(17, 2);
-            testValidator_17_2_true = new NumberValidator(17, 2, true);
+            Action res = () => { new NumberValidator(precision, scale, onlyPositive); };
+            res.ShouldThrow<ArgumentException>().WithMessage(exMessage);
         }
 
-	    [Test]
-	    public void Constructor_ThrowException_PrecisionIsZeroOrLess()
-	    {
-	        Action res = () =>
-	        {
-                var numberValidator  = new NumberValidator(0);
-	        };
 
-            	res.ShouldThrow<ArgumentException>("zero precision");
-	    }
-
-	    [Test]
-        public void Constructor_ThrowException_ScaleIsLessThenZero()
-	    {
-	        Action res = () =>
-	        {
-	            var numberValidator = new NumberValidator(0, -1);
-	        };
-
-	        res.ShouldThrow<ArgumentException>("scale is negative (-1)");
-	    }
-
-	    [Test]
-        public void Constructor_ThrowException_ScaleIsMoreThenPrecision()
-	    {
-	        Action res = () =>
-	        {
-	            var numberValidator = new NumberValidator(0, 1);
-	        };
-
-	        res.ShouldThrow<ArgumentException>("scale is more then precision (0 < 1)");
-	    }
-
-	    [Test]
-        public void Constructor_NotThrowException_PrecisionIsMoreThenZeroAndScaleIsLessThenPrecision()
-	    {
-	        Action res = () =>
-	        {
-	            var numberValidator = new NumberValidator(1);
-	        };
-
-	        res.ShouldNotThrow<ArgumentException>();
-	    }
-
-	    [Test]
-        public void IsValidNumber_ReturnsFalse_IsNull()
-	    {
-            testValidator_3_2.IsValidNumber(null).Should().BeFalse();
-	    }
-
-	    [Test]
-	    public void IsValidNumber_ReturnsFalse_IsEmpty()
-	    {
-            testValidator_3_2.IsValidNumber("").Should().BeFalse();
-	    }
-
-	    [Test]
-	    public void IsValidNumber_ReturnsFalse_IsLetters()
-	    {
-            testValidator_3_2.IsValidNumber("a.sd").Should().BeFalse();
-	    }
-
-        [Test]
-        public void IsValidNumber_ReturnsFalse_WithoutOneValue()
+        [TestCase(1, 0, false, TestName = 
+            "When precision is more then zero and scale is less then precision")]
+        public void Constructor_NotThrowException(int precision, int scale, bool onlyPositive)
         {
-            testValidator_3_2.IsValidNumber("0.").Should().BeFalse();
-            testValidator_3_2.IsValidNumber(".0").Should().BeFalse();
+            Action res = () => { new NumberValidator(precision, scale, onlyPositive); };
+            res.ShouldNotThrow<ArgumentException>();
         }
 
-        [Test]
-        public void IsValidNumber_ReturnsTrue_CommaBetweenValues()
+
+        [TestCase(3, 2, false, null, TestName = "When value is null")]
+        [TestCase(3, 2, false, "", TestName = "When value string is empty")]
+        [TestCase(3, 2, false, "a.sd", TestName = "When value from letters")]
+        [TestCase(3, 2, false, "0.", TestName = "When value without one signification (integer part)")]
+        [TestCase(3, 2, false, ".0", TestName = 
+            "When value without one signification (fractional part)")]
+        [TestCase(3, 2, false, "0.000", TestName = "When fractional part is more then scale")]
+        [TestCase(3, 2, false, "+1.23", TestName = 
+            "When integer part plus fractional part is more then precision")]
+        [TestCase(17, 2, true, "-1.23", TestName = "When only positive values and sign is negative")]
+
+        public void IsValidNumber_ReturnsFalse(int precision, int scale, bool positiveOnly, string paramValue)
         {
-            testValidator_3_2.IsValidNumber("0,00").Should().BeTrue();
+            new NumberValidator(precision, scale, positiveOnly).IsValidNumber(paramValue).Should().BeFalse();
         }
-        
-	    [Test]
-        public void IsValidNumber_ReturnsFalse_FracPartIsMoreThenScale()
-	    {
-            testValidator_17_2.IsValidNumber("0.000").Should().BeFalse();
-	    }
 
-	    [Test]
-        public void IsValidNumber_ReturnsFalse_IntPartPlusFracPartIsMoreThenPrecision()
-	    {
-	        testValidator_3_2.IsValidNumber("+1.23").Should().BeFalse();
-	    }
 
-	    [Test]
-        public void IsValidNumber_ReturnsFalse_onlyPositiveAndSignIsNegative()
-	    {
-            testValidator_17_2_true.IsValidNumber("-1.23").Should().BeFalse(); 
-	    }
+        [TestCase(17, 2, true, "+1.23", TestName = "When only positive values and sign is positive")]
+        [TestCase(3, 2, false, "0,00", TestName = "When comma is between values")]
 
-	    [Test]
-	    public void IsValidNumber_ReturnsTrue_onlyPositiveAndSignIsPositive()
-	    {
-            testValidator_17_2_true.IsValidNumber("+1.23").Should().BeTrue();
-	    }
-	}
+        public void IsValidNumber_ReturnsTrue(int precision, int scale, bool positiveOnly, string paramValue)
+        {
+            new NumberValidator(precision, scale, positiveOnly).IsValidNumber(paramValue).Should().BeTrue();
+        }
+    }
 
-	public class NumberValidator
-	{
-		private readonly Regex numberRegex;
-		private readonly bool onlyPositive;
-		private readonly int precision;
-		private readonly int scale;
+    public class NumberValidator
+    {
+        private readonly Regex numberRegex;
+        private readonly bool onlyPositive;
+        private readonly int precision;
+        private readonly int scale;
 
-		public NumberValidator(int precision, int scale = 0, bool onlyPositive = false)
-		{
-			this.precision = precision;
-			this.scale = scale;
-			this.onlyPositive = onlyPositive;
-			if (precision <= 0)
-				throw new ArgumentException("precision must be a positive number");
-			if (scale < 0 || scale >= precision)
-				throw new ArgumentException("precision must be a non-negative number less or equal than precision");
-			numberRegex = new Regex(@"^([+-]?)(\d+)([.,](\d+))?$", RegexOptions.IgnoreCase);
-		}
+        public NumberValidator(int precision, int scale = 0, bool onlyPositive = false)
+        {
+            this.precision = precision;
+            this.scale = scale;
+            this.onlyPositive = onlyPositive;
+            if (precision <= 0)
+                throw new ArgumentException("precision must be a positive number");
+            if (scale < 0 || scale >= precision)
+                throw new ArgumentException("precision must be a non-negative number less or equal than precision");
+            numberRegex = new Regex(@"^([+-]?)(\d+)([.,](\d+))?$", RegexOptions.IgnoreCase);
+        }
 
-		public bool IsValidNumber(string value)
-		{
-			// Проверяем соответствие входного значения формату N(m,k), в соответствии с правилом, 
-			// описанным в Формате описи документов, направляемых в налоговый орган в электронном виде по телекоммуникационным каналам связи:
-			// Формат числового значения указывается в виде N(m.к), где m – максимальное количество знаков в числе, включая знак (для отрицательного числа), 
-			// целую и дробную часть числа без разделяющей десятичной точки, k – максимальное число знаков дробной части числа. 
-			// Если число знаков дробной части числа равно 0 (т.е. число целое), то формат числового значения имеет вид N(m).
+        public bool IsValidNumber(string value)
+        {
+            // Проверяем соответствие входного значения формату N(m,k), в соответствии с правилом, 
+            // описанным в Формате описи документов, направляемых в налоговый орган в электронном виде по телекоммуникационным каналам связи:
+            // Формат числового значения указывается в виде N(m.к), где m – максимальное количество знаков в числе, включая знак (для отрицательного числа), 
+            // целую и дробную часть числа без разделяющей десятичной точки, k – максимальное число знаков дробной части числа. 
+            // Если число знаков дробной части числа равно 0 (т.е. число целое), то формат числового значения имеет вид N(m).
 
-			if (string.IsNullOrEmpty(value))
-				return false;
+            if (string.IsNullOrEmpty(value))
+                return false;
 
-			var match = numberRegex.Match(value);
-			if (!match.Success)
-				return false;
+            var match = numberRegex.Match(value);
+            if (!match.Success)
+                return false;
 
-			// Знак и целая часть
-			var intPart = match.Groups[1].Value.Length + match.Groups[2].Value.Length;
-			// Дробная часть
-			var fracPart = match.Groups[4].Value.Length;
+            // Знак и целая часть
+            var intPart = match.Groups[1].Value.Length + match.Groups[2].Value.Length;
+            // Дробная часть
+            var fracPart = match.Groups[4].Value.Length;
 
-			if (intPart + fracPart > precision || fracPart > scale)
-				return false;
+            if (intPart + fracPart > precision || fracPart > scale)
+                return false;
 
-			if (onlyPositive && match.Groups[1].Value == "-")
-				return false;
-			return true;
-		}
-	}
+            if (onlyPositive && match.Groups[1].Value == "-")
+                return false;
+            return true;
+        }
+    }
 }
